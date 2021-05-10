94282c618dc6b3cb5d62272120969de8dd19252c
Anand <Radhay.Anand@centurylink.com>
2021-05-11 04:15:43 +0530

    Added ADSL and VDSL

--- Utils/ExtractData.py
+++ Utils/ExtractData.py
@@ -25,7 +25,30 @@ def getADSLSummary(line):
         if var == 'Upstream  Noise Margin':  Dictionary.dict["pair1marginUp"] = val.strip()
         if var == 'Downstream  Noise Margin':  Dictionary.dict["pair1marginDn"] = val.strip()
 
+def getVDSLSummary(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if var == 'Modem temperature':  Dictionary.dict["modemTemp"] = val.strip()
+        if var == 'Upstream  Capacity':  Dictionary.dict["pair1capacityUp"] = val.strip()
+        if var == 'Downstream  Capacity':  Dictionary.dict["pair1capacityDn"] = val.strip()
+        if var == 'Upstream  Noise Margin':  Dictionary.dict["pair1marginUp"] = val.strip()
+        if var == 'Downstream  Noise Margin':  Dictionary.dict["pair1marginDn"] = val.strip()
+
 def getADSLErrors(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if (var == 'Local(Dn) LOSS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1losErrorDn'] = val.strip()
+        if (var == 'Local(Dn) FEC/min') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1fecMinErrorDn'] = val.strip()
+        if (var == 'Local(Dn) CRC/min') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1crcMinErrorDn'] = val.strip()
+        if (var == 'Local(Dn) LOFS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1lofErrorDn'] = val.strip()
+        if (var == 'Local(Dn) LOMS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1lomErrorDn'] = val.strip()
+        if (var == 'Remote(Up) FLOSS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1fLossUp'] = val.strip()
+        if (var == 'Remote(Up) FEC/min') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1fecMinErrorUp'] = val.strip()
+        if (var == 'Remote(Up) CRC/min') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1crcMinErrorUp'] = val.strip()
+        if (var == 'Remote(Up) LOFS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1lofErrorUp'] = val.strip()
+        if (var == 'Remote(Up) LOMS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1lomErrorUp'] = val.strip()
+
+def getVDSLErrors(line):
     if ':' in line:
         var, val, *rest = (line.split(":"))
         if (var == 'Local(Dn) LOSS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1losErrorDn'] = val.strip()
@@ -49,6 +72,16 @@ def getADSLPerformance(line):
         if (var == 'Local(Dn) UAS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1uasErrorDn'] = val.strip()
         if (var == 'Remote(Up) UAS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1uasErrorUp'] = val.strip()
 
+def getVDSLPerformance(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if (var == 'Local(Dn) ES') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1esErrorDn'] = val.strip()
+        if (var == 'Remote(Up) ES') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1esErrorUp'] = val.strip()
+        if (var == 'Local(Dn) SES') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1sesErrorDn'] = val.strip()
+        if (var == 'Remote(Up) SES') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1sesErrorUp'] = val.strip()
+        if (var == 'Local(Dn) UAS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1uasErrorDn'] = val.strip()
+        if (var == 'Remote(Up) UAS') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1uasErrorUp'] = val.strip()
+
 def getADSLSignal(line):
     if ':' in line:
         var, val, *rest = (line.split(":"))
@@ -60,7 +93,25 @@ def getADSLSignal(line):
         if (var == 'Upstream Connected Method') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1usConnMethod'] = val.strip()
         if (var == 'Downstream Connected Method') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1dsConnMethod'] = val.strip()
 
+def getVDSLSignal(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if (var == '1MHz Attenuation') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1oneMhzAtten'] = val.strip()
+        if (var == 'Num Syncs') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1syncCount'] = val.strip()
+        if (var == 'Sync Time') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1syncTime'] = val.strip()
+        if (var == 'Training Time') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1trainTime'] = val.strip()
+        if (var == 'Estimated Length') and (var not in Dictionary.dict.keys()): Dictionary.dict['pair1estLoopLength'] = val.strip()
+
 def getADSLIdentity(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if var == 'Modem Firmware':  Dictionary.dict["modemFirmVersion"] = val.strip()
+        if var == 'DSL Version':  Dictionary.dict["dslVersion"] = val.strip()
+        if var == 'Modem Chipset':  Dictionary.dict["modemChipset"] = val.strip()
+        if var == 'Remote Vendor Name':  Dictionary.dict["vendorName"] = val.strip()
+        if var == 'Remote Vendor ID':  Dictionary.dict["vendorId"] = val.strip()
+
+def getVDSLIdentity(line):
     if ':' in line:
         var, val, *rest = (line.split(":"))
         if var == 'Modem Firmware':  Dictionary.dict["modemFirmVersion"] = val.strip()
@@ -77,6 +128,19 @@ def getADSLDataSummary(line):
         if var == 'Emulation Type':  Dictionary.dict["dslEmulationType"] = val.strip()
         if var == 'Pri VC':  Dictionary.dict["dslPriVc"] = val.strip()
 
+def getVDSLDataSummary(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if var == 'Data Mode':  Dictionary.dict["dslDataMode"] = val.strip()
+        if var == 'Data Link':  Dictionary.dict["dslDataLink"] = val.strip()
+        if var == 'Emulation Type':  Dictionary.dict["dslEmulationType"] = val.strip()
+        if var == 'Rx Bytes':  Dictionary.dict["dslRxBytes"] = val.strip()
+        if var == 'Rx Frames':  Dictionary.dict["dslRxFrames"] = val.strip()
+        if var == 'Rx Errors':  Dictionary.dict["dslRxErrors"] = val.strip()
+        if var == 'Tx Bytes':  Dictionary.dict["dslTxBytes"] = val.strip()
+        if var == 'Tx Frames':  Dictionary.dict["dslTxFrames"] = val.strip()
+        if var == 'Tx Errors':  Dictionary.dict["dslTxErrors"] = val.strip()
+
 def getIPv4(line):
     if ':' in line:
         var, val, *rest = (line.split(":"))
@@ -88,6 +152,7 @@ def getIPv4(line):
         if var == 'DNS':  Dictionary.dict["dnsServer0"] = val.strip()
         if var == 'MAC Address':  Dictionary.dict["wanMacAddress"] = val.strip()
         if var == 'State':  Dictionary.dict["ipv4State"] = val.strip()
+        if var == 'WAN VLAN ID':  Dictionary.dict["wanVlanId"] = val.strip()
 
 def getEthernet(line):
     if ':' in line:
@@ -150,3 +215,39 @@ def getTraceRoute(line):
             Dictionary.dict["traceRtHop5Delay"] = list[0]
             Dictionary.dict["traceRtHop5Ip"] = list[1]
             Dictionary.dict["traceRtHop5Nm"] = list[2]
+
+def getVDSLVectoring(line):
+    if ':' in line:
+        var, val, *rest = (line.split(":"))
+        if var == 'Vectored Bands':  Dictionary.dict["vectorBand"] = val.strip()
+        if var == 'Vector State':  Dictionary.dict["vectorState"] = val.strip()
+        if var == 'Error Samples Sent':  Dictionary.dict["errSampleSent"] = val.strip()
+        if var == 'Error Samples Dropped':  Dictionary.dict["errSampleDrop"] = val.strip()
+        if var == 'Error Status Sent':  Dictionary.dict["errStatusSent"] = val.strip()
+        if var == 'Error Status Dropped':  Dictionary.dict["errStatusDrop"] = val.strip()
+
+def getVDSLBondStatistics(line):
+    if "US0   Loop" in line:
+        list = line.split("=")
+        Dictionary.dict["pair1up0Latn"] = list[1].split("   ")[0]
+        Dictionary.dict["pair1up0Satn"] = list[2].split("   ")[0]
+        Dictionary.dict["pair1up0Margin"] = list[3].split("   ")[0]
+        Dictionary.dict["pair1up0TxPower"] = list[4]
+    if "US1   Loop" in line:
+        list = line.split("=")
+        Dictionary.dict["pair1up1Latn"] = list[1].split("   ")[0]
+        Dictionary.dict["pair1up1Satn"] = list[2].split("   ")[0]
+        Dictionary.dict["pair1up1Margin"] = list[3].split("   ")[0]
+        Dictionary.dict["pair1up1TxPower"] = list[4]
+    if "DS1   Loop" in line:
+        list = line.split("=")
+        Dictionary.dict["pair1dn1Latn"] = list[1].split("   ")[0]
+        Dictionary.dict["pair1dn1Satn"] = list[2].split("   ")[0]
+        Dictionary.dict["pair1dn1Margin"] = list[3].split("   ")[0]
+        Dictionary.dict["pair1dn1TxPower"] = list[4]
+    if "DS2   Loop" in line:
+        list = line.split("=")
+        Dictionary.dict["pair1dn2Latn"] = list[1].split("   ")[0]
+        Dictionary.dict["pair1dn2Satn"] = list[2].split("   ")[0]
+        Dictionary.dict["pair1dn2Margin"] = list[3].split("   ")[0]
+        Dictionary.dict["pair1dn2TxPower"] = list[4]
